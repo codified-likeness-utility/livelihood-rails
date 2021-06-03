f17a07625915a63574d7b924b23cd1a1c594b45d
Ian Davies <mr.daviesian@gmail.com>
2021-06-03 00:20:11 -0500

    updating job application complete

--- livelihood/app/controllers/api/v1/jobs_controller.rb
+++ livelihood/app/controllers/api/v1/jobs_controller.rb
@@ -21,14 +21,12 @@ class Api::V1::JobsController < ApplicationController
     end
 
     def update
-        job = Job.find(params[:id])
-        job.update(params.require(:job).permit(
-            jobTitle: params['jobTitle'],
-            companyName: params['companyName'],
+        job = @user.jobs.find_by(id: params[:id])
+        job.update(
             description: params['description'],
-            jobPostUrl: params['jobPostUrl'],
-            salary: params['salary']
-        ))
+            salary: params['salary'],
+            applications_attributes: [{id: 1, status: params['status']}]
+        )
         render json: job
     end
 
@@ -46,7 +44,8 @@ class Api::V1::JobsController < ApplicationController
             :description,
             :jobPostUrl,
             :dateApplied,
-            :salary
+            :salary,
+            applications_attributes: [:id, :status]
         )
     end
     
--- livelihood/app/models/job.rb
+++ livelihood/app/models/job.rb
@@ -1,4 +1,16 @@
 class Job < ApplicationRecord
     belongs_to :user
     has_many :applications
+
+    accepts_nested_attributes_for :applications
+
+
+    def applications_attributes
+        applications_attributes.values.each do |application_attribute|
+            application = Application.find_or_create_by(pokemon_attribute)
+            self.applications << application
+        end
+    end
+    
 end
+
